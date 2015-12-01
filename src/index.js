20befc27f5b7bee7b45143ba5daef36426121b7a
Matthias Le Brun <mlbli@me.com>
2015-12-01 11:25:08 +0100

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import painAuChocolat from "pain-au-chocolat"
+
+export default function () {
+  console.error(
+    "the chocolatine package is deprecated, please use `pain-au-chocolat`"
+  )
+  return painAuChocolat()
+}
